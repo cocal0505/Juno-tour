a34dc92518c0ea267802bcc44b3d7d7684ffde62
Juno <cocal3250@gmail.com>
2021-03-30 17:42:10 +0900

    completed cloud functions password .

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,31 @@
 exports.handler = function(event, context, callback){
+   const secretContent = `
+   <h3>Welcome to the secret area</h3> 
+   <p>we can tell you that this is <strong> shit </strong> </p> 
+   `
+   
+   
+   
+    let body 
+
+   if (event.body){
+    body = JSON.parse(events.body)
+   }else{
+    body = {}
+   }
+   
+   if (body.password == "javascript"){
     callback(null,{
         statusCode :200,
-        body:"Welcome to the super secret area"
+        body:secretContent
     })
+   }else{
+    callback(null,{
+        statusCode :401  
+    })
+   }
+   
+    
 }
+
+
