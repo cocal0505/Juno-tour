d46e674f80b4da08cca43bfdfc70e4472b232d5e
Juno <cocal3250@gmail.com>
2021-03-30 17:59:33 +0900

    changed the password again.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,12 +1,14 @@
 exports.handler = function(event, context, callback){
-   const secretContent = `
+   
+   
+    const secretContent = `
    <h3>Welcome To The secret area</h3> 
-   <p>we can tell you that this is <strong>shit</strong> </p> 
+   <p>we can tell you that this is <strong>shit</strong></p> 
    `
    
    
    
-    let body 
+   let body 
 
    if (event.body){
     body = JSON.parse(event.body)
